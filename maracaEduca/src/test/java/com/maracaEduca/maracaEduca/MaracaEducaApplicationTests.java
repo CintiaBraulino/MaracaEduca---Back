9bbc2b23f57087b21c91de6f725cca426a68041e
cintia braulino <cintiabraulino@gmail.com>
2025-06-02 16:43:15 -0300

    projeto inicial

---
+++ maracaEduca/src/test/java/com/maracaEduca/maracaEduca/MaracaEducaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.maracaEduca.maracaEduca;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MaracaEducaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
